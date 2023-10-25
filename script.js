df68d4d815b30c89510205893813077861a2ee56
karthikeyan1630 <144580115+karthikeyan1630@users.noreply.github.com>
2023-10-25 23:32:23 +0530

    Add files via upload
    hi

---
+++ script.js
@@ -0,0 +1,13 @@
+var swiper = new Swiper(".mySwiper", {
+    effect: "cards",
+    grabCursor: true,
+    initialSlide: 2,
+    loop: true,
+    rotate: true,
+    mousewheel: {
+    invert: false,
+  },
+});
+function Continue(){
+	window.open("https://karthikeyan1630.github.io/Happy-Birthday3/");
+	}
